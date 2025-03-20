bb5ca67761050ea8ade45615f54f0cf9e9a365f9
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-03-20 21:01:32 +0100

    Create Trailing_Stop_and_Take-Profit.vue

---
+++ configuration/Trailing_Stop_and_Take-Profit.vue
@@ -0,0 +1,13 @@
+<template>
+  <CollapsibleSection title="Trailing Stop">
+    <NumberInput 
+      label="Activation Threshold (%)" 
+      v-model="config.order_types.stop_orders.stop_loss.activation_threshold_percent"
+      :min="0.1" :max="10" step="0.1"
+    />
+    <NumberInput
+      label="Trailing Step (%)"
+      v-model="config.order_types.stop_orders.stop_loss.trailing_step_percent"
+      :min="0.1" :max="5" step="0.1"
+    />
+  </CollapsibleSection>
