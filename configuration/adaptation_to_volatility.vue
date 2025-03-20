309ee84c3d6b82d4fe057f0f1afd8ec91f177a5a
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-03-20 21:02:02 +0100

    Create adaptation_to_volatility.vue

---
+++ configuration/adaptation_to_volatility.vue
@@ -0,0 +1,12 @@
+<VolatilityAdjustmentCard>
+  <IndicatorSelector 
+    :options="['ATR', 'Standard Deviation', 'Bollinger Bands']"
+    v-model="config.volatility_settings.metrics.primary_indicator"
+  />
+  <RangeSlider
+    label="Position Size Range"
+    :min="0.5" :max="5" 
+    v-model:lower="config.volatility_settings.adaptive_parameters.position_size_scaling.min_size_percent"
+    v-model:upper="config.volatility_settings.adaptive_parameters.position_size_scaling.max_size_percent"
+  />
+</VolatilityAdjustmentCard>
