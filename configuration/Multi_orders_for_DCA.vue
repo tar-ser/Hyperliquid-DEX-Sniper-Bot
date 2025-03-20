73251413daa89f62a93355f421565669ca8d5fd2
0xTar <166238884+tar-ser@users.noreply.github.com>
2025-03-20 21:02:28 +0100

    Create Multi_orders_for_DCA.vue

---
+++ configuration/Multi_orders_for_DCA.vue
@@ -0,0 +1,13 @@
+<DCALayersConfigurator>
+  <div v-for="(layer, index) in config.advanced_strategies.DCA.layers" :key="index">
+    <NumberInput 
+      :label="`Layer ${index+1} Price Drop (%)`" 
+      v-model="layer.price_drop_percent"
+    />
+    <NumberInput
+      :label="`Layer ${index+1} Size (%)`"
+      v-model="layer.size_percent"
+    />
+  </div>
+  <Button @click="addDCALayer">+ Add Layer</Button>
+</DCALayersConfigurator>
